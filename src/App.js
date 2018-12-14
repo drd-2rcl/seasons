92eb5c8234e96ebb226e708a46dd3a89ca1c1fa8
drd-2rcl <drd.rclrd@gmail.com>
2018-12-14 08:03:50 -0200

    :recycle: refactoring code

--- src/App.js
+++ src/App.js
@@ -27,16 +27,24 @@ class App extends Component {
         console.log('My component was just updated - it rerendered!');
     }
 
-    render() {
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage} </div>;
         }
 
         if (!this.state.errorMessage && this.state.lat) {
-            return <SeasonDisplay lat={this.state.lat}/>;
+            return <SeasonDisplay lat={this.state.lat} />;
         }
 
-        return <Spinner />;
+        return <Spinner message="Please, accept location request for to continue." />;
+    }
+
+    render() {
+       return (
+           <div className="border red">
+            {this.renderContent()}
+           </div>
+       ); 
     }
 }
 
